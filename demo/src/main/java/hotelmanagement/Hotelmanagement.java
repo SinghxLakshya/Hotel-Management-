ac5f5a7e8c113e1a0f8917232eea59689915aed3
SinghxLakshya <lakshyasingh9359@gmail.com>
2026-07-24 14:25:51 +0530

    hover effect issue resolved

--- demo/src/main/java/hotelmanagement/Hotelmanagement.java
+++ demo/src/main/java/hotelmanagement/Hotelmanagement.java
@@ -1,32 +1,38 @@
 package hotelmanagement;
-import java.awt.*;
+
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent; // ✅ Correct import
 
-import javax.swing.*;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.Timer;
 
-import org.w3c.dom.events.MouseEvent;
-
-public class Hotelmanagement extends JFrame implements ActionListener{
+public class Hotelmanagement extends JFrame implements ActionListener {
     JButton clickButton;
 
-    public Hotelmanagement(){
+    public Hotelmanagement() {
 
-        setSize(1366,695);
+        setSize(1366, 695);
         setLayout(null);
 
-        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("icons/splash4.png"));
-        JLabel image=new JLabel(i1);
-        image.setBounds(0,0,1366,695);
-        image.setSize(1366,695);
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/splash4.png"));
+        JLabel image = new JLabel(i1);
+        image.setBounds(0, 0, 1366, 695);
+        image.setSize(1366, 695);
         add(image);
 
-
-        JLabel text=new JLabel("Hotel Management System");
-        text.setBounds(10,100,500,90);
+        JLabel text = new JLabel("Hotel Management System");
+        text.setBounds(10, 100, 500, 90);
         text.setForeground(Color.white);
-        text.setFont(new Font("serif",Font.PLAIN,40));
+        text.setFont(new Font("serif", Font.PLAIN, 40));
         image.add(text);
 
         Timer timer = new Timer(500, new ActionListener() {
@@ -36,7 +42,6 @@ public class Hotelmanagement extends JFrame implements ActionListener{
             }
         });
         timer.start(); // Start the blinking effect
-        // --- BLINKING LOGIC END ---
 
         clickButton = new JButton("CLICK HERE TO CONTINUE");
         clickButton.setBounds(455, 580, 320, 40);
@@ -49,11 +54,13 @@ public class Hotelmanagement extends JFrame implements ActionListener{
 
         // Hover effect
         clickButton.addMouseListener(new MouseAdapter() {
+            @Override
             public void mouseEntered(MouseEvent evt) {
                 clickButton.setBackground(new Color(0, 120, 215));
                 clickButton.setForeground(Color.WHITE);
             }
 
+            @Override
             public void mouseExited(MouseEvent evt) {
                 clickButton.setBackground(new Color(0, 0, 0, 200));
                 clickButton.setForeground(Color.WHITE);
@@ -61,13 +68,15 @@ public class Hotelmanagement extends JFrame implements ActionListener{
         });
 
         clickButton.addActionListener(this);
+        
+       
         image.add(clickButton);
 
         setVisible(true);
     }
 
     @Override
-    public void actionPerformed(ActionEvent ae){
+    public void actionPerformed(ActionEvent ae) {
         setVisible(false);
         new Login();
     }
@@ -75,10 +84,4 @@ public class Hotelmanagement extends JFrame implements ActionListener{
     public static void main(String[] args) {
         new Hotelmanagement();
     }
-    // @Override
-    // public void actionPerformed(ActionEvent e) {
-    //     // TODO Auto-generated method stub
-    //     throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
-    // }
-   
 }
