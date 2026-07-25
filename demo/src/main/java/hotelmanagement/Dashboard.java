530c3d0f2b2b9dd50018c679dcefc179e14686fc
SinghxLakshya <lakshyasingh9359@gmail.com>
2026-07-25 12:43:45 +0530

    frame frontend completed

--- demo/src/main/java/hotelmanagement/Dashboard.java
+++ demo/src/main/java/hotelmanagement/Dashboard.java
@@ -5,25 +5,50 @@ import javax.swing.*;
 
 public class Dashboard extends JFrame {
 
+    public Dashboard() {
 
-    public Dashboard(){
-        
-        setBounds(0,0,1550,1000);
+        setBounds(0, 0, 1550, 1000);
         setLayout(null);
 
-     ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/third.jpg"));
-     Image i2=i1.getImage().getScaledInstance(1366,1000,Image.SCALE_DEFAULT);
-     ImageIcon i3=new ImageIcon(i2);
-     JLabel image=new JLabel(i3);
-     image.setBounds(0, 0, 1366, 1000);
-     add(image);
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/third.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(1366, 1000, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0, 0, 1366, 1000);
+        add(image);
 
+        // menubar is cooded here
+        JMenuBar mb = new JMenuBar();
+        mb.setBounds(0, 0, 1366, 30);
+        image.add(mb);
 
+        JMenu hotel = new JMenu("Hotel Management");
+        mb.add(hotel);
 
-     // menubar is cooded here
+        JMenu ad = new JMenu("Admin");
+        ad.setForeground(Color.RED);
+        mb.add(ad);
+
+        // Menu itemsare here
+        JMenuItem recep=new JMenuItem("Reception");
+        recep.setForeground(new Color(0, 123, 255));
+        hotel.add(recep);
+
+       JMenuItem ademp=new JMenuItem("Add employee");
+        ademp.setForeground(new Color(0, 123, 255));
+        ad.add(ademp);
+
+       JMenuItem adrms=new JMenuItem("Add rooms");
+        adrms.setForeground(new Color(0, 123, 255));
+        ad.add(adrms);
         
+       JMenuItem addrv=new JMenuItem("Add Drives");
+        addrv.setForeground(new Color(0, 123, 255));
+        ad.add(addrv);
+
         setVisible(true);
-        }
+    }
+
     public static void main(String[] args) {
         new Dashboard();
     }
