4b4354d46e0eb7e8fe72eadeb82d28591e53e2b8
SinghxLakshya <lakshyasingh9359@gmail.com>
2026-07-26 13:27:14 +0530

    add employee frame created

---
+++ demo/src/main/java/hotelmanagement/Addemp.java
@@ -0,0 +1,19 @@
+package hotelmanagement;
+import java.awt.*;
+
+import javax.swing.*;
+
+public class Addemp extends JFrame{
+public Addemp(){
+    setLayout(null);
+    setBounds(300,100,850,540);
+    setVisible(true);
+}
+
+
+
+    public static void main(String[] args) {
+        new Addemp();
+    }
+    
+}
