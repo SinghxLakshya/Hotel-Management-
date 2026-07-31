0738747bd8c323a6b7ba0e58cb561a7cf064dff4
SinghxLakshya <lakshyasingh9359@gmail.com>
2026-07-31 23:47:08 +0530

    frame created

--- demo/src/main/java/hotelmanagement/Customer.java
+++ demo/src/main/java/hotelmanagement/Customer.java
@@ -1,5 +1,22 @@
 package hotelmanagement;
 
-public class Customer {
+import javax.swing.JFrame;
+import java.awt.*;
+
+public class Customer extends JFrame
+ {
+   public Customer(){
+      setLayout(null);
+        setBounds(300, 100, 850, 560);
+        getContentPane().setBackground(Color.WHITE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setVisible(true);
+   } 
+   public static void main(String [] args){
+    new Customer();
     
+
+   }
+   
 }
